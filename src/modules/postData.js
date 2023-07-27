dedb4d3808d28601189863f91e898933d9f74518
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-07-27 19:37:21 +0300

    Fix linters

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,7 +1,7 @@
 const postData = async (obj) => {
-  const content = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/M34398fsnvkkjbjfjis/scores/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const content = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/M34398fsnvkkjbjfjis/scores/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       user: obj.name,
       score: obj.score,
